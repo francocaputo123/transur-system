c72e4ccc84475cc22b35584e5f41d12bdf0cc8ea
Gian Franco Miguel Caputo <gianfrancocap12@gmail.com>
2025-05-05 15:30:39 -0300

    Added add cylinder form ad logic for database

---
+++ src/main/java/controllers/cylinder_controller.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import com.mycompany.transur.system.Cconection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class cylinder_controller {
+    
+    public static void addCylinder(String type, int serial, String state, int number){
+        Cconection conect = new Cconection();
+        
+        String sql = "INSERT INTO cylinder (type,serial,state,number) VALUES (?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = conect.conect().prepareStatement(sql);
+            ps.setString(1,type);
+            ps.setInt(2,serial);
+            ps.setString(3, state);
+            ps.setInt(4, number);
+            
+            ps.execute();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error", "Hubo un error al resgistrar e cilindro: " + e.toString(), JOptionPane.ERROR_MESSAGE);
+        }
+        
+        finally {
+            conect.closeConnection();
+        }
+    }
+}
