d77d9bac583acde53da5cf2648d013f31b8f7d74
Gian Franco Miguel Caputo <gianfrancocap12@gmail.com>
2025-05-16 18:32:20 -0300

    Added history cylinders

---
+++ src/main/java/controllers/CylinderHistoryController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import com.mycompany.transur.system.Cconection;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CylinderHistoryController {
+    
+    public static void showCylinderHistory() {
+       
+        Cconection conect = new Cconection(); 
+        
+        String sql = "SELECT * FROM ";
+    }
+    
+}
